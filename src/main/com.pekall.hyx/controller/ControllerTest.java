ff6b9b258ab1311c47977a6977a1b788ee9a8cea
YingxiaoHe <290519965@qq.com>
2017-11-03 17:11:52 +0800

    sync

---
+++ src/main/com.pekall.hyx/controller/ControllerTest.java
@@ -0,0 +1,22 @@
+package main.com.pekall.hyx.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@EnableAutoConfiguration
+public class ControllerTest {
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String sayHello() {
+        return "Hello World";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControllerTest.class, args);
+    }
+}
